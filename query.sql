04734e7388911e005173e4b5f645f51d26a420e8
bouqrt <bouchraelqrt@gmail.com>
2025-12-03 16:38:51 +0100

    last queries

--- query.sql
+++ query.sql
@@ -34,4 +34,52 @@ WHERE balance <= 0;
 SELECT * FROM transactions
 WHERE transaction_type = 'debit';
 
+-- Transactions de type "credit"
+SELECT * FROM transactions
+WHERE transaction_type = 'credit';
+
+-- Transactions du account_id = 1
+SELECT * FROM transactions
+WHERE account_id = 1;
+
+-- Customers ayant un account géré par l’advisor_id = 2
+SELECT DISTINCT c.* FROM customers c JOIN accounts a ON c.id = a.customer_id WHERE a.advisor_id = 2;
+
+-- Accounts ayant account_type = "Savings"
+SELECT * FROM accounts WHERE account_type = 'savings';
+
+-- Transactions avec amount ≥ 500
+SELECT * FROM transactions WHERE amount >= 500;
+
+-- Transactions amount entre 100 et 1000
+SELECT * FROM transaction WHERE amount BETWEEN 100 AND 1000;
+
+-- Accounts du customer_id = 1
+SELECT * FROM accounts WHERE customer_id = 1;
+
+-- Accounts triés par balance (croissant)
+SELECT * FROM accounts ORDER BY balance ASC;
+
+-- Transactions triées par amount (décroissant)
+SELECT * FROM transactions ORDER BY amount DESC;
+
+-- Les 5 plus grandes transactions
+SELECT * FROM transactions ORDER BY amount DESC LIMIT 5;
+
+-- Toutes les transactions triées par date décroissante
+SELECT * FROM transactions ORDER BY transaction_date DESC;
+
+-- Les 3 dernières transactions
+SELECT * FROM transactions ORDER BY transaction_date DESC LIMIT 3;
+
+-- Chaque account avec le nom du customer et du advisor
+SELECT 
+    a.id AS account_id,
+    a.account_number,
+    a.balance,
+    c.full_name AS customer_name,
+    ad.full_name AS advisor_name
+FROM accounts a
+JOIN customers c ON a.customer_id = c.id
+JOIN advisors ad ON a.advisor_id = ad.id;
 
